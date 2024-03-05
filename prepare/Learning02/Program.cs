fcf915ec3db835cb84e4e12d502aec2d6b137403
O-J2315 <jhernandezap23@gmail.com>
2024-03-05 09:38:43 -0700

    Week 2 created a resume and job classes, then in the main programm we created 2 instances for jobs and one for a resume. Then displayed the information.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company="Microsoft",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+        Job job2 = new Job
+        {
+            _jobTitle = "Manager",
+            _company="Apple",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        Resume resume1 = new Resume();
+        resume1._name = "Josue Hernandez";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
+
     }
 }
