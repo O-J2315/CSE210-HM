f3d3c74e3d2381e1a704dfd68e1b13acb198ec59
O-J2315 <jhernandezap23@gmail.com>
2024-03-13 14:54:25 -0600

    Arrangements to the display menu in the Program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,16 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the scripture memorizer!");
-
         Reference theReference = new Reference("1 Nefi", 3, 7, 8);
         Scripture theScripture = new Scripture(theReference,"And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
-
 
         string ans = "";
         while (ans!="quit"){
             Console.Clear();
             Console.WriteLine(theScripture.GetDisplayText());
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
             ans = Console.ReadLine();
             if(theScripture.IsCompletelyHidden() || ans=="quit"){
                             break;
