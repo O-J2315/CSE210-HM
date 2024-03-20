6982870707a182fd3e8dae37a5a1be56bf99d65a
O-J2315 <jhernandezap23@gmail.com>
2024-03-20 09:06:31 -0600

    Example Done for articulating

---
+++ Car.cs
@@ -0,0 +1,6 @@
+public class Car{
+    private int _vinNumber;
+    private string _maker;
+    privater string _model;
+    private int _year;
+}
---
+++ Truck.cs
@@ -0,0 +1,5 @@
+public class Truck : Car{
+    private float _towingCapacity;
+    private int _bedWidth;
+    private int _bedHeight;
+}
