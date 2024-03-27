3fbee7d2fdc52dbad867882b5ae0dd247e0ae871
O-J2315 <jhernandezap23@gmail.com>
2024-03-27 13:52:52 -0600

    eraesed uneccesary things

--- Car.cs
+++
@@ -1,6 +0,0 @@
-public class Car{
-    private int _vinNumber;
-    private string _maker;
-    privater string _model;
-    private int _year;
-}
--- Truck.cs
+++
@@ -1,5 +0,0 @@
-public class Truck : Car{
-    private float _towingCapacity;
-    private int _bedWidth;
-    private int _bedHeight;
-}
