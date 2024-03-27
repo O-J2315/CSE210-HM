7c48015903581e5c66cf0769495f0e9522847a70
O-J2315 <jhernandezap23@gmail.com>
2024-03-27 16:58:46 -0600

    Also formated the consolo logs to be more visualy attractive and easy to manipulate. Added the spinner animation and made it go automatic. Comments for extra credit added to the program.cs file.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,3 +1,7 @@
+//Added for extra credit animations done in past assignments
+//Added leveling up behavior to the goal manager class.
+//Added a way to handle exceptions and mistakes when no goals have been created or selected.
+//When the user levels up a message appears and new level name is given.
 using System;
 class Program
 {
